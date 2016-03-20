a25b8b81b895584492373911c845d1a827138528
DOCTOR_RABB <doctor.rabb@mail.ru>
2016-03-20 20:05:30 +0400

    Update start.py

--- start.py
+++ start.py
@@ -17,7 +17,7 @@ def main ():
     microl_server.init ()
     
     cfg_str = ''
-    conf = open ('/usr/bin/microl/config.json')
+    conf = open ('/etc/microl/config.json')
     
     
     for i in conf.readlines():
